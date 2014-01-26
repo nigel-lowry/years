8b69cd1a17677fcd9119b5962ccbd92480c0693e
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2014-01-26 18:01:36 +0000

    Refactor

--- lib/years.rb
+++ lib/years.rb
@@ -7,10 +7,6 @@ module Years
   def self.range first_year, last_year=Time.now.year
     raise if first_year > last_year
 
-    if last_year == first_year
-      first_year.to_s
-    else
-      first_year.to_s + @@EN_DASH + last_year.to_s
-    end
+    (last_year == first_year) ? first_year.to_s : first_year.to_s + @@EN_DASH + last_year.to_s
   end
 end
