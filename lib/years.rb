c3c010884121403f8f1ff4ec3eb9c97e5f4ecf42
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2014-01-26 17:50:37 +0000

    Rename

--- lib/years.rb
+++ lib/years.rb
@@ -4,13 +4,13 @@ module Years
 
   @@EN_DASH = '–'
 
-  def self.range year
+  def self.range first_year
     current_year = Time.now.year
 
-    if current_year == year
-      year.to_s
+    if current_year == first_year
+      first_year.to_s
     else
-      year.to_s + @@EN_DASH + current_year.to_s
+      first_year.to_s + @@EN_DASH + current_year.to_s
     end
   end
 end
