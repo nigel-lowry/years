089f5a4bf5e2574ceeba28897d18546a7516f6f8
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2014-05-31 19:21:15 +0100

    Convert strings

--- spec/years_spec.rb
+++ spec/years_spec.rb
@@ -4,11 +4,11 @@ require 'active_support/all'
 require 'years'
 
 describe Years do
-  describe ".age_where_leapling_legal_birthday_is_1_mar" do
-    context "regular birthday" do
+  describe '.age_where_leapling_legal_birthday_is_1_mar' do
+    context 'regular birthday' do
       let(:date_of_birth) { '22 Sep 1976'.to_date }
 
-      it "raises error if not born yet" do
+      it 'raises error if not born yet' do
         expect { Years.age_where_leapling_legal_birthday_is_1_mar(date_of_birth, '21 Sep 1976'.to_date) }.to raise_error
       end
 
@@ -23,14 +23,14 @@ describe Years do
       specify { expect(Years.age_where_leapling_legal_birthday_is_1_mar date_of_birth, '22 Sep 1978'.to_date).to eq 2 }
       specify { expect(Years.age_where_leapling_legal_birthday_is_1_mar date_of_birth, '23 Sep 1978'.to_date).to eq 2 }
 
-      it "uses current date when second argument missing" do
+      it 'uses current date when second argument missing' do
         Timecop.freeze '22 Sep 2006'.to_date do
           expect(Years.age_where_leapling_legal_birthday_is_1_mar date_of_birth).to eq 30
         end
       end
     end
 
-    context "leap year birthday" do
+    context 'leap year birthday' do
       let(:date_of_birth) { '29 Feb 2004'.to_date }
 
       specify { expect(Years.age_where_leapling_legal_birthday_is_1_mar date_of_birth, '1 Mar 2004'.to_date).to eq 0 }
@@ -39,12 +39,12 @@ describe Years do
     end
   end
 
-  describe ".age_where_leapling_legal_birthday_is_28_feb" do
+  describe '.age_where_leapling_legal_birthday_is_28_feb' do
     let(:date_of_birth) { '29 Feb 2004'.to_date }
     specify { expect(Years.age_where_leapling_legal_birthday_is_28_feb date_of_birth, '28 Feb 2005'.to_date).to eq 1 }
   end
 
-  describe ".range" do
+  describe '.range' do
     before do
       Timecop.freeze '26 Jan 2012'.to_date
     end
@@ -57,7 +57,7 @@ describe Years do
     specify { expect(Years.range 2011).to eq '2011–2012' }
     specify { expect(Years.range 2012).to eq '2012' }
 
-    it "raises error if range would be in future" do
+    it 'raises error if range would be in future' do
       expect { Years.range(2013) }.to raise_error
     end
 
@@ -66,7 +66,7 @@ describe Years do
     specify { expect(Years.range 2011, 2013).to eq '2011–2013' }
     specify { expect(Years.range 2011, 2014).to eq '2011–2014' }
 
-    it "raises error if range is wrong way round" do
+    it 'raises error if range is wrong way round' do
       expect { Years.range(2013, 2011) }.to raise_error
     end
   end
