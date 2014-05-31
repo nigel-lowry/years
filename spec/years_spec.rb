fb83f968fe2e2012a75b9a4480467e7aec994351
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2014-05-31 19:19:23 +0100

    Convert to expect

--- spec/years_spec.rb
+++ spec/years_spec.rb
@@ -53,18 +53,18 @@ describe Years do
       Timecop.return
     end
 
-    specify { Years.range(2010).should == '2010–2012' }
-    specify { Years.range(2011).should == '2011–2012' }
-    specify { Years.range(2012).should == '2012' }
+    specify { expect(Years.range 2010).to eq '2010–2012' }
+    specify { expect(Years.range 2011).to eq '2011–2012' }
+    specify { expect(Years.range 2012).to eq '2012' }
 
     it "raises error if range would be in future" do
       expect { Years.range(2013) }.to raise_error
     end
 
-    specify { Years.range(2011, 2011).should == '2011' }
-    specify { Years.range(2011, 2012).should == '2011–2012' }
-    specify { Years.range(2011, 2013).should == '2011–2013' }
-    specify { Years.range(2011, 2014).should == '2011–2014' }
+    specify { expect(Years.range 2011, 2011).to eq '2011' }
+    specify { expect(Years.range 2011, 2012).to eq '2011–2012' }
+    specify { expect(Years.range 2011, 2013).to eq '2011–2013' }
+    specify { expect(Years.range 2011, 2014).to eq '2011–2014' }
 
     it "raises error if range is wrong way round" do
       expect { Years.range(2013, 2011) }.to raise_error
